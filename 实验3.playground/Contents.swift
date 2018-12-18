005f7a31d94702f31db1e7127d90954c2481c723
Lynalmost <lynxx0526@gmail.com>
2018-12-18 14:28:57 +0800

    实验三

---
+++ 实验3.playground/Contents.swift
@@ -0,0 +1,94 @@
+import Foundation
+
+//let currentDate = Date()
+//var formatter = DateFormatter()
+//formatter.dateFormat = "yyyy 年 M 月 dd 日 EEEE aa KK:mm"
+//formatter.locale = Locale(identifier: "zh_CN")
+
+//formatter.timeZone = TimeZone(abbreviation: "UTC+8:00")
+//let beijing = formatter.string(from: currentDate)
+//print("Beijing time:" + beijing)
+
+//formatter.timeZone = TimeZone(abbreviation: "UTC+9:00")
+//let tokyo = formatter.string(from: currentDate)
+//print("Tokyog time:" + tokyo)
+
+//formatter.timeZone = TimeZone(abbreviation: "UTC-5:00")
+//let newyork = formatter.string(from: currentDate)
+//print("NewYork time:" + newyork)
+//formatter.timeZone = TimeZone(abbreviation: "UTC+0:00")
+
+//let london = formatter.string(from: currentDate)
+//print("London time:" + london)
+
+//var str = "Swift is a powerful and intuitive programming language for iOS, OS X, tvOS, and watchOS.";
+//分别找到起始点和结束点
+//let index1 = str.index(str.startIndex, offsetBy: 6)
+//let index2 = str.index(str.startIndex, offsetBy: 21)
+
+//let str1 = str[index1..<index2]
+//print(str1)
+
+//let str2 = str.replacingOccurrences(of:"OS", with:"")
+
+//task 1
+import Foundation
+
+let currentDate = Date()
+var formatter = DateFormatter()
+formatter.dateFormat = "yyyy 年 M 月 dd 日 EEEE aa KK:mm"
+formatter.locale = Locale(identifier: "zh_CN")
+
+formatter.timeZone = TimeZone(abbreviation: "UTC+8:00")
+let beijing = formatter.string(from: currentDate)
+print("Beijing time:" + beijing)
+
+formatter.timeZone = TimeZone(abbreviation: "UTC+9:00")
+let tokyo = formatter.string(from: currentDate)
+print("Tokyog time:" + tokyo)
+
+formatter.timeZone = TimeZone(abbreviation: "UTC-5:00")
+let newyork = formatter.string(from: currentDate)
+print("NewYork time:" + newyork)
+formatter.timeZone = TimeZone(abbreviation: "UTC+0:00")
+
+let london = formatter.string(from: currentDate)
+print("London time:" + london)
+//task 2
+
+var str = "Swift is a powerful and intuitive programming language for iOS, OS X, tvOS, and watchOS.";
+//分别找到起始点和结束点
+let index1 = str.index(str.startIndex, offsetBy: 6)
+let index2 = str.index(str.startIndex, offsetBy: 21)
+
+let str1 = str[index1..<index2]
+print(str1)
+
+let str2 = str.replacingOccurrences(of:"OS", with:"")
+//task 3
+print(str2)
+let dic = ["date": ["Beijing": beijing, "Tokyo": tokyo, "NewYork": newyork, "London":london, "subString": str1]] as! AnyObject  //将字典转换为任意类型，便于后面写入文件
+let defaultDoc = FileManager.default  //获取默认工作路径
+
+//获取工作路径下的Document文件夹
+let fileManager = FileManager.default
+var path = fileManager.urls(for: .documentDirectory, in:.userDomainMask).first!
+
+path.appendPathComponent("dic.txt")
+
+try? dic.write(to: path, atomically: true)
+
+import UIKit
+
+let imageUrl = URL("string:https://ss0.baidu.com/7Po3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=bdb1172ebd7eca800d053fe7a1229712/5fdf8db1cb134954068359695b4e9258d1094a19.jpg”)
+
+let urlData = try Data(contentsOf:imageUrl!)    //将json数据转换为二进制数据
+
+let fileManager = FileManager.default
+
+var path = fileManager.urls(for: .documentDirectory, in:.userDomainMask).first!
+
+path.appendPathComponent(“girl.jpg”)
+print(path)
+try urlData.write(to: path)
+
